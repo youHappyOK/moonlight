6e1fb65ebec2c90960f63c9f8abbc434ce25ccf4
YouHappyOK <luojin_mail@163.com>
2023-11-20 15:53:21 +0800

    1

--- input/ClickGaussian.py
+++ input/ClickGaussian.py
@@ -13,7 +13,8 @@ import numpy as np
 
 class ClickGaussian:
 
-    def generatePointInCircle(self, centerX, centerY, radius):
+    @staticmethod
+    def generatePointInCircle(centerX, centerY, radius):
         while True:
             x = np.random.normal(centerX, radius / 3)
             y = np.random.normal(centerY, radius / 3)
@@ -24,10 +25,9 @@ class ClickGaussian:
 if __name__ == '__main__':
     img = np.zeros((400, 400, 3), dtype=np.uint8)
     for i in range(1000):
-        clickGaussian = ClickGaussian()
         centerX, centerY = 200, 200  # 圆心坐标
         radius = 150  # 圆半径
-        x, y = clickGaussian.generatePointInCircle(centerX, centerY, radius)
+        x, y = ClickGaussian.generatePointInCircle(centerX, centerY, radius)
         cv2.circle(img, (x, y), 2, (0, 255, 0), -1)
         cv2.imshow('Gaussian Distribution', img)
         cv2.waitKey(1)
